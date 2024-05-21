56a3097dfbddf431da624d3def5efff28b4d8dd6
Federico Yarza <federicoyarza295@gmail.com>
2024-05-21 07:05:32 -0400

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
-  base: "https://fyarza.github.io/portafolio",
+  base: "https://fyarza.github.io/portafolio-web",
 });
