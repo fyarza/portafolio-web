112575bb57715f6b65c42ce1f5edaf1fde75a1ef
Federico Yarza <federicoyarza295@gmail.com>
2024-05-21 07:48:39 -0400

    ajuste de javascript en linea

--- main.js
+++
@@ -1,6 +0,0 @@
-// import { toggleTheme } from "./source.js";
-
-// window.toogleTheme = toggleTheme;
-
-import { greet } from "./source.js";
-window.greetFromModule = greet;
--- source.js
+++
@@ -1,7 +0,0 @@
-export const toggleTheme = () => {
-  document.body.classList.toggle("dark");
-};
-
-export function greet() {
-  alert("Hello from module");
-}
